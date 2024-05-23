058374cbc0d07b93b21bdd38b93ff6be7d62dd02
Ronald Arias <ariasron@hotmail.com>
2024-05-23 14:49:05 -0500

    creates card skeleton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import './App.css'
 
+function CardSkeleton() {
+  return <div className="max-w-md m-auto my-10 flex">
+    <div className="flex flex-col bg-slate-100 py-3 px-2 rounded gap-1">
+      <div className="bg-slate-200 rounded w-32 h-20"/>
+      <div className="flex flex-row gap-1 items-center">
+        <div className="bg-slate-200 rounded-full w-8 h-8"/>
+        <div className="flex flex-col gap-1">
+          <div className="bg-slate-200 rounded w-16 h-3"/>
+          <div className="bg-slate-200 rounded w-20 h-2"/>
+        </div>
+      </div>
+    </div>
+  </div>;
+}
+
 function App() {
 
   return (
-    <h1 className="text-xl">Hello World</h1>
+    <div className="max-w-md m-auto my-10 flex gap-2">
+      <CardSkeleton/>
+      <CardSkeleton/>
+      <CardSkeleton/>
+    </div>
   )
 }
 
