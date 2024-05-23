7ca3738b73fb89eb6fabb7bdc386554209b40dd8
Ronald Arias <ariasron@hotmail.com>
2024-05-23 14:50:41 -0500

    adds animation to card skeleton component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function CardSkeleton() {
   return <div className="max-w-md m-auto my-10 flex">
-    <div className="flex flex-col bg-slate-100 py-3 px-2 rounded gap-1">
+    <div className="flex flex-col bg-slate-100 py-3 px-2 rounded gap-1 animate-pulse">
       <div className="bg-slate-200 rounded w-32 h-20"/>
       <div className="flex flex-row gap-1 items-center">
         <div className="bg-slate-200 rounded-full w-8 h-8"/>
